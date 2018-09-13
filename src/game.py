db2c5bff63d5829df6b35eab6b5bba7f6212663f
X. HU(Mac) <i@finalobject.cn>
2018-09-13 15:42:56 +0800

    add detailed annotation

--- src/game.py
+++ src/game.py
@@ -17,11 +17,14 @@ class Game:
         self.bg = PhotoImage(file='../image/background.gif')
         w = self.bg.width()
         h = self.bg.height()
+        # 设置游戏背景
         for x in range(0,5):
             for y in range(0,5):
                 self.canvas.create_image(x*w ,y*h ,image=self.bg,anchor='nw')
+        # 游戏所以的精灵储存在这里
         self.sprites = []
         self.running = True
+    # 运行的时候就执行这个主循环，只要running为True，小人就会不停移动下去
     def mainloop(self):
         while 1:
             if self.running == True:
@@ -30,8 +33,9 @@ class Game:
             self.tk.update_idletasks()
             self.tk.update()
             time.sleep(0.01)
+# 初始化游戏，填入精灵
 game = Game()
-sp = Sprite(game)
+# 所以的平台精灵
 platform1 = PlatformSprite(game,PhotoImage(file='../image/platform3.gif'),0,480,100,10)
 platform2 = PlatformSprite(game,PhotoImage(file='../image/platform3.gif'),150,440,100,10)
 platform3 = PlatformSprite(game,PhotoImage(file='../image/platform3.gif'),300,400,100,10)
@@ -42,6 +46,7 @@ platform7 = PlatformSprite(game,PhotoImage(file='../image/platform2.gif'),170,12
 platform8 = PlatformSprite(game,PhotoImage(file='../image/platform2.gif'),45,60,60,10)
 platform9 = PlatformSprite(game,PhotoImage(file='../image/platform1.gif'),170,250,30,10)
 platform10 = PlatformSprite(game,PhotoImage(file='../image/platform1.gif'),230,200,30,10)
+# 初始化之后加入sprites数组中
 game.sprites.append(platform1)
 game.sprites.append(platform2)
 game.sprites.append(platform3)
@@ -52,8 +57,11 @@ game.sprites.append(platform7)
 game.sprites.append(platform8)
 game.sprites.append(platform9)
 game.sprites.append(platform10)
+# 主角
 stick = StickSprite(game)
 game.sprites.append(stick)
+# 门
 door = DoorSprite(game,PhotoImage(file='../image/door1.gif'),PhotoImage(file='../image/door2.gif'),45,30,40,35)
 game.sprites.append(door)
+# 启动游戏
 game.mainloop()
--- src/sprite.py
+++ src/sprite.py
@@ -3,34 +3,49 @@
 from coords import *
 from tkinter import *
 import time
+# 这个叫做精灵🧚‍♀️，哈哈我也不知道为什么叫做这个，就是用来表示一个窗口里面的各个元素，作为之后一些元素的父类
 class Sprite:
     def __init__(self,game):
         self.game = game
+        # 这个东西都判断游戏结束的标签，只有小人抵达终点才能是True，所以只有在代表终点的门里，这个endGame才会等于True
         self.endgame = False
+        # 坐标
         self.coordinates = None
+    #一般的精灵都是不会的，所以默认都是pass
     def move(self):
         pass
     def coords(self):
         return self.coordinates
 class DoorSprite(Sprite):
+    # 输入的参数就是两个不同状态下门的图片，关门和开门，默认关门，之后小人抵达门的时候才会变成开门状态
+    # 别的参数就是坐标，宽度，高度
     def __init__(self,game,photo_image1,photo_image2,x,y,width,height):
         Sprite.__init__(self,game)
         self.x = x
         self.y = y
         self.photo_image1 = photo_image1
         self.photo_image2 = photo_image2
+        # 这句话就可以在画布上创建一个门，并把这个门的图像返回给self.image，之后需要修改图片的时候操作这个image就行，如果是自己直接创建，就会导致原来的门还在，又出现了一个新的门。
         self.image = game.canvas.create_image(x,y,image = self.photo_image1,anchor='nw')
         self.coordinates = Coords(x,y,x+(width/2),y+height)
+        # 门是游戏的重点！所以门的endGame是True
         self.endgame = True
+    # 当小人碰到门的时候，执行这个函数，从image1变到image2，这是门精灵特有的函数
+    def changeEndImage(self):
+        self.game.canvas.itemconfig(self.image,image=self.photo_image2)
+# 这个就是平台，游戏里可以承载小人的平台
 class PlatformSprite(Sprite):
     def __init__(self,game,photo_image,x,y,width,height):
         Sprite.__init__(self,game)
         self.photo_image = photo_image
         self.image = game.canvas.create_image(x,y,image=self.photo_image,anchor='nw')
         self.coordinates = Coords(x,y,x+width,y+height)
+# 这个就是火柴人啦，我们的主角
 class StickSprite(Sprite):
     def __init__(self,game):
         Sprite.__init__(self,game)
+        # 小人图片是提前设定好的，而不是初始化的时候输入的
+        # 分别就是向左跑的三个动作和向右跑的三个动作
         self.images_left = [
             PhotoImage(file='../image/stick-L1.gif'),
             PhotoImage(file='../image/stick-L2.gif'),
@@ -41,14 +56,19 @@ class StickSprite(Sprite):
             PhotoImage(file='../image/stick-R2.gif'),
             PhotoImage(file='../image/stick-R3.gif')
         ]
+        # 初始化是默认为向右跑的第一个动作
         self.image = game.canvas.create_image(0,450,image=self.images_left[0],anchor='nw')
-        self.x = -2
+        # 初始速度
+        self.x = 0
         self.y = 0
+        # 当前图像
         self.current_image = 0
+        # 图片变换的方向，步长
         self.current_image_add = 1
         self.jump_count = 0
         self.last_time = time.time()
         self.coordinates = Coords()
+        # 绑定操作
         game.canvas.bind_all('<KeyPress-Left>',self.turn_left)
         game.canvas.bind_all('<KeyPress-Right>',self.turn_right)
         game.canvas.bind_all('<space>',self.jump)
@@ -58,29 +78,39 @@ class StickSprite(Sprite):
     def turn_right(self,evt):
         #if self.y == 0:
         self.x = 2
+    #跳跃函数
     def jump(self,evt):
+        #只有在没有跳跃的状态才能进行起跳，防止二连跳
         if self.y ==0:
             self.y = -4
             self.jump_count = 0
     def animate(self):
         if self.x != 0 and self.y ==0:
+            #每0.1s更换一下图片，刷新自身图片
+            #不把刷新做在整体的update里面，可以让不同精灵拥有不同的刷新频率
             if time.time() - self.last_time >0.1:
                 self.last_time = time.time()
                 self.current_image += self.current_image_add
+                #图片更换的顺序为1->2->3->2->1->2...这样连贯一点
                 if self.current_image >=2:
                     self.current_image_add =-1
                 if self.current_image <=0:
                     self.current_image_add =1
+        # 如果速度方向向左
         if self.x<0:
+            # 但是如果在起跳状态下，直接是固定的起跳图片
             if self.y!=0:
                 self.game.canvas.itemconfig(self.image,image=self.images_left[2])
+            # 不是的话，那就在朝左的图片中依次变换
             else:
                 self.game.canvas.itemconfig(self.image,image=self.images_left[self.current_image])
+        # 和速度向左同理
         elif self.x>0:
             if self.y!=0:
                 self.game.canvas.itemconfig(self.image,image=self.images_right[2])
             else:
                 self.game.canvas.itemconfig(self.image,image=self.images_right[self.current_image])
+    # 小人的体积都是预设的
     def coords(self):
         xy = self.game.canvas.coords(self.image)
         self.coordinates.x1 = xy[0]
@@ -88,20 +118,29 @@ class StickSprite(Sprite):
         self.coordinates.x2 = xy[0]+27
         self.coordinates.y2 = xy[1]+30
         return self.coordinates
+    # 小人的move应该就是整个精髓所在了
     def move(self):
+        #首先根据现有状态更新动画，可以放在开头，也可以放在函数最后
         self.animate()
+        # 如果小人处于上升状态，那么会不停的计数，等到一定时间之后，小人的速度会向下，模拟重力嘛，但是速度不是连续变化的
         if self.y <0:
             self.jump_count += 1
             if self.jump_count >20:
                 self.y =4
-        if self.y >0:
-            self.jump_count -= 1
+        # 下面这个判断之前代码的这么写的，但是今天写备注的时候发现没啥意义，注释之后同样正常工作
+        # if self.y >0:
+        #     self.jump_count -= 1
+        # 获取自身坐标，为接下来的碰撞检测做准备
         co = self.coords()
+        # 四个方向是否可以继续走！
+        # 为True表示这个方向没有碰到边界，为False表示触碰到了边界，可以是画布边界，也可以是其他精灵的边界
         left = True
         right = True
         top = True
         bottom = True
+        # 是否正在下落
         falling = True
+        # 下面四个if是用来判断和画布边缘碰撞的
         if self.y >0 and co.y2 >=self.game.canvas_height:
             self.y = 0
             bottom = False
@@ -114,6 +153,7 @@ class StickSprite(Sprite):
         elif self.x <0 and co.x1 <=0:
             self.x =0
             left = False
+        # for循环开始判断和其他精灵的碰撞
         for sprite in self.game.sprites:
             if sprite == self:
                 continue
@@ -134,13 +174,15 @@ class StickSprite(Sprite):
             if left and self.x <0 and collided_left(co,sprite_co):
                 self.x=0
                 left = False
+                # 这个是判断游戏标志，如果碰触到了endgame为True的，running就会呗设置为False，小人就不能跑了，认为游戏结束。同时，对应的这个精灵，其实就是门，也需要执行changeEndImage，修改为开门状态。
                 if sprite.endgame:
                     self.game.running = False
-                    sprite.game.canvas.itemconfig(sprite.image,image=sprite.photo_image2)
+                    sprite.changeEndImage()
             if right and self.x >0 and collided_right(co,sprite_co):
                 self.x=0
                 right = False
         if falling and bottom and self.y ==0 \
            and co.y2 < self.game.canvas_height:
             self.y=4
+        # 根据速度移动位置
         self.game.canvas.move(self.image,self.x,self.y)
